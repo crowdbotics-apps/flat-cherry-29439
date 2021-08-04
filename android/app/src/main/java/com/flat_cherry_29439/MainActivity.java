1893c6878a25a21ee2d9ac904661442e2832a566
Crowdbotics <team@crowdbotics.com>
2021-08-04 03:29:16 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/flat_cherry_29439/MainActivity.java
@@ -0,0 +1,15 @@
+package com.flat_cherry_29439;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "flat_cherry_29439";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "flat_cherry_29439Identifier",
+  url: "https://flat_cherry_29439Identifier.botics.co",
+  api: "https://flat_cherry_29439Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
